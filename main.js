85277a16a26cc8987b9044e448f028ef3827b79d
Roya1Flush <dima-dimon25@mail.ru>
2024-07-31 23:15:14 +0500

    JuleCom

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { Telegraf, Markup } from 'telegraf'
 import {message} from 'telegraf/filters'
 
+
 const token = '7169691857:AAFFV34W2MAeHUN4ucVH7GHYVRtN7Qm6ggs'
 const webAppUrl = 'https://yammyfoot.web.app/' 
-
 const bot = new Telegraf(token)
 
 bot.command('start', (ctx) => {
@@ -19,5 +19,5 @@ bot.command('start', (ctx) => {
     const data = ctx.webAppData.data.json()
     ctx.reply(`Ваше обращение принято в работу, ожидайте обратной связи в течение 24 часов!: ${data?.feedback}` ?? 'empty message')
   })
-  
+
   bot.launch()
